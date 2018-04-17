b6a60783796e1ed7e2a661be0fabdbc37f5dd567
Raghavendranath Kandula <raghavendranath.kandula525@gmail.com>
2018-04-17 08:10:34 -0500

    added state variable for game playing

--- app.js
+++ app.js
@@ -9,51 +9,61 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
-    //1. Random number
-    var dice = Math.floor(Math.random()*6) + 1;
+    if(gamePlaying){
+        //1. Random number
+        var dice = Math.floor(Math.random()*6) + 1;
     
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-'+dice+'.png';
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-'+dice+'.png';
     
     
-    //3. Update the round score if the rolled number was not a 1
-    if(dice !== 1){
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-'+activePlayer).textContent = roundScore;
-    } else {
-        //Next Player
-        nextPlayer();
+        //3. Update the round score if the rolled number was not a 1
+        if(dice !== 1){
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-'+activePlayer).textContent = roundScore;
+        } else {
+            //Next Player
+            nextPlayer();
+        }
     }
+    
+   
 }); 
 
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    //Add current score to global score
-    scores[activePlayer]  += roundScore;
     
-    //Update the UI
-    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+    if(gamePlaying){
+        //Add current score to global score
+        scores[activePlayer]  += roundScore;
     
-    //Check if player won the game
-    if(scores[activePlayer] >= 100){
-        document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
-        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+        //Update the UI
+        document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+    
+        //Check if player won the game
+        if(scores[activePlayer] >= 100){
+            document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
         
-    } else {
-        //Next Player
-        nextPlayer();
+            gamePlaying = false;
+
+        } else {
+            //Next Player
+            nextPlayer();
+        }
     }
+    
 });
 
 function nextPlayer(){
@@ -84,7 +94,7 @@ function init(){
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
-    
+    gamePlaying = true;
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -98,4 +108,5 @@ function init(){
     //We don't want two active classes. So remove it and add
     document.querySelector('.player-0-panel').classList.add('active');
     document.querySelector('.player-1-panel').classList.remove('active');
+
 }
